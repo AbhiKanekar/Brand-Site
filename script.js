210727d25d119a6ffc136beb93faccb591cb7f7f
AbhiKanekar <mrunmaykanekar@gmail.com>
2024-10-04 16:02:47 +0530

    script.js

--- script.js
+++ script.js
@@ -130,6 +130,6 @@ let pageFourAnimation = () => {
 }
 
 pageFourAnimation();
-// pageThreeAnimation();
-// pageTwoAnimation();
-// pageOneAnimation();
+pageThreeAnimation();
+pageTwoAnimation();
+pageOneAnimation();
